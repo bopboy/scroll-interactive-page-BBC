0e0f7b0950f24d4e5d31f9e2c7311141f6689d75
bopboy <39391891+bopboy@users.noreply.github.com>
2022-05-31 05:31:10 +0900

    07 코드 개선 (intersectionObserver 사용)

--- main.js
+++ main.js
@@ -2,8 +2,15 @@
     const stepElems = document.querySelectorAll('.step')
     const graphicElems = document.querySelectorAll('.graphic-item')
     let currentItem = graphicElems[0]
+    let ioIndex
+
+    const io = new IntersectionObserver((entries, observer) => {
+        ioIndex = entries[0].target.dataset.index * 1
+        console.log(ioIndex)
+    })
 
     for (let i = 0; i < stepElems.length; i++) {
+        io.observe(stepElems[i])
         // stepElems[i].setAttribute('data-index', i)
         stepElems[i].dataset.index = i
         graphicElems[i].dataset.index = i
@@ -19,8 +26,9 @@
         let step
         let boundingRect
 
-        for (let i = 0; i < stepElems.length; i++) {
+        for (let i = ioIndex - 1; i < ioIndex + 2; i++) {
             step = stepElems[i]
+            if (!step) continue
             boundingRect = step.getBoundingClientRect()
 
             if (boundingRect.top >= window.innerHeight * 0.1 &&
