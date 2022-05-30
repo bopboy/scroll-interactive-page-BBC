b322a3436e18b761762dc679d44158e9c6a7f021
bopboy <39391891+bopboy@users.noreply.github.com>
2022-05-31 06:09:04 +0900

    09 새 이동 애니메이션

--- main.js
+++ main.js
@@ -3,7 +3,20 @@
     const graphicElems = document.querySelectorAll('.graphic-item')
     let currentItem = graphicElems[0]
     let ioIndex
-
+    const actions = {
+        birdFlies(key) {
+            if (key)
+                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(${window.innerWidth}px)`
+            else
+                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(-100%)`
+        },
+        birdFlies2(key) {
+            if (key)
+                document.querySelector('[data-index="5"] .bird').style.transform = `translate(${window.innerWidth}px, ${-window.innerHeight * 0.7}px)`
+            else
+                document.querySelector('[data-index="5"] .bird').style.transform = `translateX(-100%)`
+        }
+    }
     const io = new IntersectionObserver((entries, observer) => {
         ioIndex = entries[0].target.dataset.index * 1
         console.log(ioIndex)
@@ -16,11 +29,13 @@
         graphicElems[i].dataset.index = i
     }
 
-    function activate() {
+    function activate(action) {
         currentItem.classList.add('visible')
+        if (action) { actions[action](true) }
     }
-    function inactivate() {
+    function inactivate(action) {
         currentItem.classList.remove('visible')
+        if (action) { actions[action](false) }
     }
     window.addEventListener('scroll', () => {
         let step
@@ -31,13 +46,13 @@
             if (!step) continue
             boundingRect = step.getBoundingClientRect()
 
-            if (boundingRect.top >= window.innerHeight * 0.1 &&
-                boundingRect.top < window.innerHeight * 0.8) {
-                inactivate()
+            if (boundingRect.top >= window.innerHeight * 0.1 && boundingRect.top < window.innerHeight * 0.8) {
+                inactivate(currentItem.dataset.action)
                 currentItem = graphicElems[step.dataset.index]
-                activate()
+                activate(currentItem.dataset.action)
             }
         }
     })
+    window.addEventListener('load', () => { setTimeout(() => scrollTo(0, 0), 100) })
     activate()
 })()
